a4ac11039a4e6c870f9d0886f02d0c6bd3cabf0b
Niyog Ray <niyog.ray@gmail.com>
2017-11-13 20:08:48 +0530

    Passing Data Around

--- Homepwner/Homepwner/ItemsViewController.swift
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -177,4 +177,21 @@ class ItemsViewController: UITableViewController {
             return proposedDestinationIndexPath
         }
     }
+    
+    // MARK: - Segues
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "showItem"?:
+            // Figure out which row was just tapped
+            if let row = tableView.indexPathForSelectedRow?.row {
+                // Get the item associated with this row and pass it along
+                let item = itemStore.allItems[row]
+                let detailViewController = segue.destination as! DetailViewController
+                detailViewController.item = item
+            }
+        default:
+            preconditionFailure("Unexpected segue identifier.")
+        }
+    }
 }

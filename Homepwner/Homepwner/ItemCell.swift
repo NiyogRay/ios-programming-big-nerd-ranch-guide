a2312ad4123306afe67cd1474a3a69cff7c288b6
Niyog Ray <niyog.ray@gmail.com>
2017-11-13 15:51:03 +0530

    ItemCell - added extra constraints to fix localization issues

--- Homepwner/Homepwner/ItemCell.swift
+++ Homepwner/Homepwner/ItemCell.swift
@@ -2,5 +2,7 @@ import UIKit
 
 class ItemCell: UITableViewCell {
 
-    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
 }

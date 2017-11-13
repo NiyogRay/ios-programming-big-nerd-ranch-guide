a5d56111e257e607c0293f06fa8f96b4ef2782eb
Niyog Ray <niyog.ray@gmail.com>
2017-11-13 16:44:26 +0530

    Responding to user changes

--- Homepwner/Homepwner/ItemCell.swift
+++ Homepwner/Homepwner/ItemCell.swift
@@ -5,4 +5,13 @@ class ItemCell: UITableViewCell {
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var serialNumberLabel: UILabel!
     @IBOutlet var valueLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // Also available in Attributes Inspector > Dynamic Type > Automatically Adjusts Font
+//        nameLabel.adjustsFontForContentSizeCategory = true
+//        serialNumberLabel.adjustsFontForContentSizeCategory = true
+//        valueLabel.adjustsFontForContentSizeCategory = true
+    }
 }

0b26b390763ca01f3b204df716d4f0b16d3cf27a
Niyog Ray <niyog.ray@gmail.com>
2017-11-28 16:29:32 +0530

    URLComponents

---
+++ Photorama/Photorama/FlickrAPI.swift
@@ -0,0 +1,48 @@
+import Foundation
+
+enum Method: String {
+    case interestingPhotos = "flickr.interestingness.getList"
+}
+
+struct FlickrAPI {
+    
+    private static let baseURLString = "https://api.flickr.com/services/rest"
+    private static let apiKey = "a6d8194499131071f158fd740860a5a88"
+    
+    private static func flickrURL(method: Method,
+                                  parameters: [String: String]?) -> URL {
+        
+        var components = URLComponents(string: baseURLString)!
+        
+        // Create queryItems
+        var queryItems = [URLQueryItem]()
+        
+        // Add baseParams
+        let baseParams = [
+            "method": method.rawValue,
+            "format": "json",
+            "nojsoncallback": "1",
+            "api_key": apiKey
+        ]
+        for (key, value) in baseParams {
+            let item = URLQueryItem(name: key, value: value)
+            queryItems.append(item)
+        }
+        
+        // Add additionalParams
+        if let additionalParams = parameters {
+            for (key, value) in additionalParams {
+                let item = URLQueryItem(name: key, value: value)
+                queryItems.append(item)
+            }
+        }
+        components.queryItems = queryItems
+        
+        // URL from components
+        return components.url!
+    }
+    
+    static var interestingPhotosURL: URL {
+        return flickrURL(method: .interestingPhotos, parameters: ["extras" : "url_h, date_taken"])
+    }
+}
